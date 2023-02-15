97252bca34b03c00b36ac5e5be81c01012a0fde7
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-15 12:22:24 +0400

    feat: create main page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+const outDir = resolve(__dirname, 'dist')
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	base: '/FCCFront/',
+	build: {
+		outDir,
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve('index.html'),
+				'src/randomQuote/': resolve('src', 'randomQuote', 'index.html'),
+			},
+		},
+	},
 })
