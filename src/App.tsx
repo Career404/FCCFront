4be2d8c21d6d5d80bd8cb9d48135c07bf3cb442a
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-23 16:27:06 +0400

    feat: main page styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ function App() {
 		<div className="App">
 			<h1>Take a look at these pages:</h1>
 			<div className="links">
-				<a href="src/randomQuote/">Random quote generator</a> -{' '}
+				<a href="src/randomQuote/">Random quote generator</a>{' '}
 				<a href="https://random-quote-machine.freecodecamp.rocks/">
 					FCC example
 				</a>
 			</div>
 			<div className="links">
-				<a href="src/pomodoro/">Random quote generator</a> -{' '}
+				<a href="src/pomodoro/">25 and 5 clock</a>{' '}
 				<a href="https://25--5-clock.freecodecamp.rocks/">FCC example</a>
 			</div>
 		</div>
