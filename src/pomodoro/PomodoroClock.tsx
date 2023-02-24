68a21a3e513677c8de192c2bc570b06d48cc39d2
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-24 15:54:48 +0400

    feat: add functional 25+5 clock

--- src/pomodoro/PomodoroClock.tsx
+++ src/pomodoro/PomodoroClock.tsx
@@ -1,17 +1,140 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-//import { } from 'react-icons/fa'
+import { FiPlay, FiPause, FiRefreshCw } from 'react-icons/fi'
+import NumberControl from './NumberControl'
 import './PomodoroClock.css'
 
 export default function PomodoroClock() {
+	const [breakLength, setBreakLength] = useState<number>(5)
+	const [sessionLength, setSessionLength] = useState<number>(25)
+	const [isSession, setIsSession] = useState<boolean>(true)
+	const [timeLeft, setTimeLeft] = useState<number>(25 * 60 * 1000)
+	const [timerInterval, setTimerInterval] = useState<ReturnType<
+		typeof setInterval
+	> | null>(null)
+	const [isRunning, setIsRunning] = useState<boolean>(false)
+	const beep = new Audio('/BeepSound.wav')
+
+	const timerRun = () => {
+		setTimerInterval(
+			setInterval(() => {
+				setTimeLeft((prevTimeLeft) => prevTimeLeft - 1000)
+			}, 100)
+		)
+		setIsRunning(true)
+	}
+	const timerPause = () => {
+		if (timerInterval) {
+			clearInterval(timerInterval)
+			setTimerInterval(null)
+			setIsRunning(false)
+		}
+	}
+	const handleReset = () => {
+		clearInterval(timerInterval!)
+		setBreakLength(5)
+		setSessionLength(25)
+		setIsSession(true)
+		setTimeLeft(25 * 60 * 1000)
+		setTimerInterval(null)
+		setIsRunning(false)
+	}
+	const timerOver = () => {
+		timerPause()
+		beep.addEventListener(
+			'canplaythrough',
+			() => {
+				beep.play()
+			},
+			{ once: true }
+		)
+		setTimeout(() => beep.pause(), 1000)
+		setIsSession(!isSession)
+		timerRun()
+	}
+	const formatTime = (time: number): string => {
+		const minutes = Math.floor(time / 60000)
+		const seconds = ((time % 60000) / 1000).toFixed(0)
+		return `${minutes}:${Number(seconds) < 10 ? '0' : ''}${seconds}`
+	}
+
+	useEffect(() => {
+		if (timeLeft <= 0) {
+			timerOver()
+		}
+	}, [timeLeft])
+
+	useEffect(() => {
+		if (isRunning) {
+			timerPause()
+		}
+		isSession
+			? setTimeLeft(sessionLength * 60 * 1000)
+			: setTimeLeft(breakLength * 60 * 1000)
+	}, [sessionLength, breakLength])
+
+	useEffect(() => {
+		console.log('session: ' + isSession)
+		if (!isSession && isRunning) {
+			timerPause()
+			setTimeLeft(breakLength * 60 * 1000)
+			timerRun()
+		} else if (isSession && isRunning) {
+			timerPause()
+			setTimeLeft(sessionLength * 60 * 1000)
+			timerRun()
+		} else
+			isSession
+				? setTimeLeft(sessionLength * 60 * 1000)
+				: setTimeLeft(breakLength * 60 * 1000)
+	}, [isSession])
+
 	return (
-		<div>
-			<h1>Pomodoro Clock</h1>
+		<div id="container">
+			<h1 className="main-title">Pomodoro Clock</h1>
+			<div className="controls">
+				<NumberControl
+					defaultValue={breakLength}
+					minValue={1}
+					maxValue={60}
+					title="Break"
+					callback={setBreakLength}
+				/>
+				<button
+					className="sessionSwitch"
+					onClick={() => setIsSession(!isSession)}
+				>
+					{isSession === true ? ' Take a break ' : 'Ready for the session'}
+				</button>
+
+				<NumberControl
+					defaultValue={sessionLength}
+					minValue={1}
+					maxValue={60}
+					title="Session"
+					callback={setSessionLength}
+				/>
+			</div>
+			<div className="timer">
+				<div id="timer-label">{isSession ? 'Session' : 'Break'}</div>
+				<div id="time-left">{formatTime(timeLeft)}</div>
+			</div>
+			<div className="timer-control">
+				<button
+					id="start_stop"
+					onClick={() => (isRunning ? timerPause() : timerRun())}
+				>
+					<FiPlay />
+					<FiPause />
+				</button>
+				<span> &nbsp; </span>
+				<button id="reset" onClick={handleReset}>
+					<FiRefreshCw />
+				</button>
+			</div>
 		</div>
 	)
 }
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<PomodoroClock />
-	</React.StrictMode>
+	<PomodoroClock />
 )
